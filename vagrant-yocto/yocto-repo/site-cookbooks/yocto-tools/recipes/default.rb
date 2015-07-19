49c9109a2a119fa1f79c4aaa1a0a58000ad7bd7f
Masayuki Kimura <masayuki.kimura.1986@gmail.com>
2015-07-20 01:35:38 +0900

    Func: update default.rb for vagrant-yocto

--- vagrant-yocto/yocto-repo/site-cookbooks/yocto-tools/recipes/default.rb
+++ vagrant-yocto/yocto-repo/site-cookbooks/yocto-tools/recipes/default.rb
@@ -25,14 +25,14 @@ packages.each do |pkg|
   end
 end
 
-git "/home/vagrant/" do
+git "/home/vagrant/poky" do
   repository "git://git.yoctoproject.org/poky"
   revision "master"
   enable_submodules true
   action :sync
 end
 
-git "/home/vagrant/poky" do
+git "/home/vagrant/poky/meta-openembedded" do
   repository "git://git.openembedded.org/meta-openembedded"
   revision "master"
   enable_submodules true
@@ -40,7 +40,7 @@ git "/home/vagrant/poky" do
 end
 
 
-git "/home/vagrant/poky" do
+git "/home/vagrant/poky/meta-linaro" do
   repository "git://git.linaro.org/openembedded/meta-linaro"
   revision "master"
   enable_submodules true
@@ -48,9 +48,47 @@ git "/home/vagrant/poky" do
 end
 
 
-git "/home/vagrant/poky" do
+git "/home/vagrant/poky/meta-xilinx" do
   repository "git://git.yoctoproject.org/meta-xilinx"
   revision "master"
   enable_submodules true
   action :sync
+end
+
+
+execute "Format Build environment" do
+  cwd "/home/vagrant/poky/"
+  command "./oe-init-build-env"
+  action :run
+end
+
+bash "conf/bblayers.conf settings" do
+  cwd "/home/vagrant/poky/build/conf/"
+  code "sed -i 's$/home/vagrant/poky/meta-yocto-bsp$/home/vagrant/poky/meta-yocto-bsp \
+                  /home/vagrant/poky/meta-linaro/meta-linaro \
+                  /home/vagrant/poky/meta-openembedded/toolchain-layer \
+                  /home/vagrant/poky/meta-xilinx $g' bblayers.conf"
+  action :run
+end
+
+bash "conf/local.conf settings" do
+  cwd "/home/vagrant/poky/build/conf/"
+  code "sed -i 's/#BB_NUMBER_THREADS/BB_NUMBER_THREADS/g' local.conf &&
+        sed -i 's/#PARALLEL_MAKE/PARALLEL_MAKE/g' local.conf &&
+        sed -i 's/MACHINE ??= \"qemux86\"/MACHINE ?= \"zedboard-zynq7\"/g' local.conf &&
+        sed -i 's/debug-tweaks/debug-tweaks tools-sdk/g' local.conf"
+  action :run
+end
+
+bash "meta-linaro/meta-linaro/conf/layer.conf settings" do
+  cwd "/home/vagrant/poky/meta-linaro/meta-linaro/conf/"
+  code "sed -i 's/LAYERDEPENDS_linaro/#LAYERDEPENDS_linaro/g' layer.conf &&
+        echo 'LAYERDEPENDS_linaro-toolchain = \"meta-networking\"' >> layer.conf"
+  action :run
+end
+
+bash "Run bitbake" do
+  cwd "/home/vagrant/poky/build/"
+  code "bitbake core-image-minimal"
+  action :run
 end
