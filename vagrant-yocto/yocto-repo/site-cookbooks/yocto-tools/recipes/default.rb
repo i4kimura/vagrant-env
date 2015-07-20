db5067f8be7777f9440cea72684fe653e79d0029
Masayuki Kimura <masayuki.kimura.1986@gmail.com>
2015-07-20 22:55:30 +0900

    Func: modify Vagrant for building yocto

--- vagrant-yocto/yocto-repo/site-cookbooks/yocto-tools/recipes/default.rb
+++ vagrant-yocto/yocto-repo/site-cookbooks/yocto-tools/recipes/default.rb
@@ -17,7 +17,7 @@ execute "update package index" do
   action :nothing
 end.run_action(:run)
 
-packages = %w{gawk wget git-core diffstat unzip texinfo build-essential libsdl1.2-dev emacs}
+packages = %w{gawk wget git-core diffstat unzip texinfo build-essential chrpath libsdl1.2-dev emacs}
 
 packages.each do |pkg|
   package pkg do
@@ -26,6 +26,7 @@ packages.each do |pkg|
 end
 
 git "/home/vagrant/poky" do
+  user "vagrant"
   repository "git://git.yoctoproject.org/poky"
   revision "master"
   enable_submodules true
@@ -33,6 +34,7 @@ git "/home/vagrant/poky" do
 end
 
 git "/home/vagrant/poky/meta-openembedded" do
+  user "vagrant"
   repository "git://git.openembedded.org/meta-openembedded"
   revision "master"
   enable_submodules true
@@ -41,6 +43,7 @@ end
 
 
 git "/home/vagrant/poky/meta-linaro" do
+  user "vagrant"
   repository "git://git.linaro.org/openembedded/meta-linaro"
   revision "master"
   enable_submodules true
@@ -49,6 +52,7 @@ end
 
 
 git "/home/vagrant/poky/meta-xilinx" do
+  user "vagrant"
   repository "git://git.yoctoproject.org/meta-xilinx"
   revision "master"
   enable_submodules true
@@ -56,22 +60,33 @@ git "/home/vagrant/poky/meta-xilinx" do
 end
 
 
+# bash "Format Build environment" do
+#   user "vagrant"
+#   cwd "/home/vagrant/poky/"
+#   code "source ./oe-init-build-env &&
+#         cd /home/vagrant/poky/build/conf/ &&
+#         sed -i 's$/home/vagrant/poky/meta-yocto-bsp$/home/vagrant/poky/meta-yocto-bsp \
+#                   /home/vagrant/poky/meta-linaro/meta-linaro \
+#                   /home/vagrant/poky/meta-openembedded/toolchain-layer \
+#                   /home/vagrant/poky/meta-xilinx $g' bblayers.conf &&
+#         cd /home/vagrant/poky/build/conf/ &&
+#         sed -i 's/#BB_NUMBER_THREADS/BB_NUMBER_THREADS/g' local.conf &&
+#         sed -i 's/#PARALLEL_MAKE/PARALLEL_MAKE/g' local.conf &&
+#         sed -i 's/MACHINE ??= \"qemux86\"/MACHINE ?= \"zedboard-zynq7\"/g' local.conf &&
+#         sed -i 's/debug-tweaks/debug-tweaks tools-sdk/g' local.conf &&
+#         cd /home/vagrant/poky/meta-linaro/meta-linaro/conf/ &&
+#         sed -i 's/LAYERDEPENDS_linaro/#LAYERDEPENDS_linaro/g' layer.conf &&
+#         echo 'LAYERDEPENDS_linaro-toolchain = \"meta-networking\"' >> layer.conf &&
+#         cd /home/vagrant/poky/build/ &&
+#         bitbake core-image-minimal"
+#   action :run
+# end
+
+
 bash "Format Build environment" do
-  cwd "/home/vagrant/poky/"
-  code "source ./oe-init-build-env &&
-        cd /home/vagrant/poky/build/conf/ &&
-        sed -i 's$/home/vagrant/poky/meta-yocto-bsp$/home/vagrant/poky/meta-yocto-bsp \
-                  /home/vagrant/poky/meta-linaro/meta-linaro \
-                  /home/vagrant/poky/meta-openembedded/toolchain-layer \
-                  /home/vagrant/poky/meta-xilinx $g' bblayers.conf &&
-        cd /home/vagrant/poky/build/conf/ &&
-        sed -i 's/#BB_NUMBER_THREADS/BB_NUMBER_THREADS/g' local.conf &&
-        sed -i 's/#PARALLEL_MAKE/PARALLEL_MAKE/g' local.conf &&
-        sed -i 's/MACHINE ??= \"qemux86\"/MACHINE ?= \"zedboard-zynq7\"/g' local.conf &&
-        sed -i 's/debug-tweaks/debug-tweaks tools-sdk/g' local.conf &&
-        cd /home/vagrant/poky/meta-linaro/meta-linaro/conf/ &&
-        sed -i 's/LAYERDEPENDS_linaro/#LAYERDEPENDS_linaro/g' layer.conf &&
-        echo 'LAYERDEPENDS_linaro-toolchain = \"meta-networking\"' >> layer.conf &&
+  user "vagrant"
+  code "cd /home/vagrant/poky/ &&
+        source ./oe-init-build-env &&
         cd /home/vagrant/poky/build/ &&
         bitbake core-image-minimal"
   action :run
