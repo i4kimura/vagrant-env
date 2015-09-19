b15b808401173f899efb8e3514a5bfd32869e993
Masayuki Kimura <masayuki.kimura.1986@gmail.com>
2015-09-19 23:06:46 +0900

    Func: update mips49 environment

--- vagrant-mips52/mips52-repo/site-cookbooks/mips52-tools/recipes/default.rb
+++ vagrant-mips52/mips52-repo/site-cookbooks/mips52-tools/recipes/default.rb
@@ -16,7 +16,11 @@ execute "sed apt-source" do
   command "sed -i -e 's%http://archive.ubuntu.com/ubuntu%http://ftp.iij.ad.jp/pub/linux/ubuntu/archive%g' /etc/apt/sources.list"
 end.run_action(:run)
 
-packages = %w{g++ bison flex libmpc-dev  libmpfr-dev libgmp-dev texinfo libexpat1-dev libncurses5-dev cmake libxml2-dev python-dev swig doxygen subversion libedit-dev git libtool automake libhidapi-dev libusb-1.0-0-dev graphviz gawk gtkterm}
+packages = %w{g++ bison flex libmpc-dev  libmpfr-dev libgmp-dev texinfo libexpat1-dev
+              libncurses5-dev cmake libxml2-dev python-dev swig doxygen subversion
+              libedit-dev git libtool automake libhidapi-dev libusb-1.0-0-dev
+              graphviz gawk gtkterm
+              liblua5.2-dev libbfd-dev binutils-dev}
 packages.each do |pkg|
   package pkg do
     action [:install, :upgrade]
@@ -48,17 +52,6 @@ directory "#{binutils_build_dir}" do
   mode '0755'
   action :create
 end
-
-cookbook_file "#{Chef::Config['file_cache_path']}/binutils-2.25/ld/ldmain.patch" do
-  source "ldmain.patch"
-end
-
-
-execute "apply ldmain patch" do
-  cwd "#{Chef::Config['file_cache_path']}/binutils-2.25/ld/"
-  command "patch --ignore-whitespace < ldmain.patch"
-end
-
 
 execute "build binutils" do
   cwd "#{binutils_build_dir}"
@@ -119,6 +112,121 @@ end
 execute "build gcc-2" do
   cwd "#{gcc_build_dir}"
   command "rm * -rf && ../configure --target=#{target} --disable-nls --disable-libssp --with-gnu-ld --with-gnu-as --disable-shared --enable-languages=\"c c++\" --with-newlib"
+  action :run
+end
+
+directory "/home/vagrant/software" do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
+
+# Install CMake
+remote_file "/home/vagrant/software/cmake-3.3.2.tar.gz" do
+  source "http://www.cmake.org/files/v3.3/cmake-3.3.2.tar.gz"
+end
+execute "build-cmake" do
+  cwd "/home/vagrant/software/"
+  command "tar xfz ./cmake-3.3.2.tar.gz && cd cmake-3.3.2/ && ./configure && make && make install"
+  action :run
+end
+
+
+# Install Swimmer-MIPS
+git "/home/vagrant/swimmer_mips" do
+  repository "https://github.com/msyksphinz/swimmer_riscv.git"
+  revision "master"
+  action :sync
+end
+
+execute "build-swimmer-mips" do
+  cwd "/home/vagrant/swimmer_mips"
+  command "git submodule init &&
+           git submodule update &&
+           cd vendor/gflags  # for building Google Flags
+           cmake .
+           make"
+  action :run
+end
+
+
+execute "build-swimmer-mips" do
+  cwd "/home/vagrant/swimmer_mips/build_mips"
+  command "cmake . && make clean && make"
+  action :run
+end
+
+
+# Install U-boot-MIPS
+git "/home/vagrant/u-boot-mips" do
+  repository "git://git.denx.de/u-boot-mips.git"
+  revision "master"
+  action :sync
+end
+
+
+# Install XV6-MIPS
+git "/home/vagrant/xv6-mips" do
+  repository "https://github.com/msyksphinz/xv6-mips.git"
+  revision "master"
+  action :sync
+end
+# Install XV6-x86
+git "/home/vagrant/xv6-public" do
+  repository "https://github.com/mit-pdos/xv6-public.git"
+  revision "master"
+  action :sync
+end
+
+
+# Install Benchmark
+git "/home/vagrant/benchmarks" do
+  repository "https://github.com/msyksphinz/benchmarks.git"
+  revision "master"
+  action :sync
+end
+
+
+# Download MIPS official gcc
+
+remote_file "/home/vagrant/software/Codescape.GNU.Tools.Package.2015.01-7.for.MIPS.MTI.Linux.CentOS-5.x86_64.tar.gz" do
+  source "http://codescape-mips-sdk.imgtec.com/components/toolchain/2015.01-7/Codescape.GNU.Tools.Package.2015.01-7.for.MIPS.MTI.Linux.CentOS-5.x86_64.tar.gz"
+end
+
+remote_file "/home/vagrant/software/Codescape.GNU.Tools.Package.2015.01-7.for.MIPS.IMG.Linux.CentOS-5.x86_64.tar.gz" do
+  source "http://codescape-mips-sdk.imgtec.com/components/toolchain/2015.01-7/Codescape.GNU.Tools.Package.2015.01-7.for.MIPS.IMG.Linux.CentOS-5.x86_64.tar.gz"
+end
+
+remote_file "/home/vagrant/software/Codescape.GNU.Tools.Package.2015.01-7.for.MIPS.MTI.Bare.Metal.CentOS-5.x86_64.tar.gz" do
+  source "http://codescape-mips-sdk.imgtec.com/components/toolchain/2015.01-7/Codescape.GNU.Tools.Package.2015.01-7.for.MIPS.MTI.Bare.Metal.CentOS-5.x86_64.tar.gz"
+end
+
+remote_file "/home/vagrant/software/Codescape.GNU.Tools.Package.2015.01-7.for.MIPS.IMG.Bare.Metal.CentOS-5.x86_64.tar.gz" do
+  source "http://codescape-mips-sdk.imgtec.com/components/toolchain/2015.01-7/Codescape.GNU.Tools.Package.2015.01-7.for.MIPS.IMG.Bare.Metal.CentOS-5.x86_64.tar.gz"
+end
+
+execute "extract MTI Linux" do
+  cwd "/home/vagrant/software"
+  command "tar xfz Codescape.GNU.Tools.Package.2015.01-7.for.MIPS.MTI.Linux.CentOS-5.x86_64.tar.gz"
+  action :run
+end
+
+execute "extract MTI Baremetal" do
+  cwd "/home/vagrant/software"
+  command "tar xfz Codescape.GNU.Tools.Package.2015.01-7.for.MIPS.MTI.Bare.Metal.CentOS-5.x86_64.tar.gz"
+  action :run
+end
+
+execute "extract IMG Linux" do
+  cwd "/home/vagrant/software"
+  command "tar xfz Codescape.GNU.Tools.Package.2015.01-7.for.MIPS.IMG.Linux.CentOS-5.x86_64.tar.gz"
+  action :run
+end
+
+execute "extract MTI Baremetal" do
+  cwd "/home/vagrant/software"
+  command "tar xfz Codescape.GNU.Tools.Package.2015.01-7.for.MIPS.IMG.Bare.Metal.CentOS-5.x86_64.tar.gz"
   action :run
 end
 
