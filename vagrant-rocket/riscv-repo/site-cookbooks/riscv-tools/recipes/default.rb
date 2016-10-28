a8dbe644dda49535ce54e8075ad4eab4881132e8
Masayuki Kimura <masayuki.kimura.1986@gmail.com>
2016-10-29 02:07:58 +0900

    Func: implement vagrant-rocket

---
+++ vagrant-rocket/riscv-repo/site-cookbooks/riscv-tools/recipes/default.rb
@@ -0,0 +1,76 @@
+#
+# Cookbook Name:: riscv-tools
+# Recipe:: default
+#
+# Copyright 2015, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+execute "sed apt-source" do
+  command "sed -i -e 's%http://archive.ubuntu.com/ubuntu%http://ftp.iij.ad.jp/pub/linux/ubuntu/archive%g' /etc/apt/sources.list"
+end.run_action(:run)
+
+execute "update package index" do
+  command "apt-get update"
+  ignore_failure true
+  action :nothing
+end.run_action(:run)
+
+packages = %w{autoconf automake autotools-dev curl libmpc-dev libmpfr-dev libgmp-dev gawk build-essential bison flex texinfo gperf gcc libc6-dev pkg-config bridge-utils uml-utilities zlib1g-dev libglib2.0-dev autoconf automake libtool libsdl1.2-dev emacs git default-jre default-jdk}
+
+packages.each do |pkg|
+  package pkg do
+    action [:install, :upgrade]
+  end
+end
+
+git "/home/vagrant/rocket-chip" do
+  repository "https://github.com/ucb-bar/rocket-chip.git"
+  enable_submodules true
+  action :sync
+  timeout 3600
+  user "vagrant"
+  group "vagrant"
+end
+
+execute "Set Environment of RISCV-tools" do
+  cwd "/home/vagrant/rocket-chip/riscv-tools/"
+  command "git submodule update --init --recursive"
+  action :run
+  user "vagrant"
+  group "vagrant"
+end
+
+ENV['TOP']   = "/home/vagrant/"
+ENV['RISCV'] = "/home/vagrant/riscv"
+ENV['PATH']  = "/home/vagrant/riscv/bin:#{ENV["PATH"]}"
+
+execute "Build RISCV-tools" do
+  cwd "/home/vagrant/rocket-chip/riscv-tools/"
+  command "./build.sh"
+  action :run
+  user "vagrant"
+  group "vagrant"
+end
+
+
+execute "Build RISCV-32bit tools" do
+  cwd "/home/vagrant/rocket-chip/riscv-tools/"
+  command "./build-rv32ima.sh"
+  action :run
+  user "vagrant"
+  group "vagrant"
+end
+
+
+#
+# Building BOOM
+#
+execute "Build RISCV-tools" do
+  cwd "/home/vagrant/rocket-chip/vsim/"
+  command "make verilog CONFIG=DefaultFPGAConfig"
+  action :run
+  user "vagrant"
+  group "vagrant"
+end
